080a8fe7943fd2accbff48e4a0b4ddd5b5708542
Juan Sáez García <contacto@juamber.com>
2021-12-25 13:16:05 +0100

    aaa_variables

---
+++ aaa_variables/main.js
@@ -0,0 +1,15 @@
+var nombre = "Oscar"
+
+//Declaramos variable
+var edad
+//Inicializamnos la variable 
+edad = 30
+
+//Array
+var elemento = ["Computador","Celular"]
+
+//Objeto
+var persona = {
+    nombre: "Diego",
+    edad: 19
+}
