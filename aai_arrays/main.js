3c7bd7491ccd9f4071503f3852c77331211f3944
Juan Sáez García <contacto@juamber.com>
2021-12-25 15:33:00 +0100

    aai_arrays

---
+++ aai_arrays/main.js
@@ -0,0 +1,18 @@
+var frutas = ["Manzana","Plátano","Cereza","Fresa"]
+console.log(frutas)
+console.log(frutas[0])
+
+frutas.push("Uvas")
+console.log(frutas)
+
+frutas.pop("Uvas")
+console.log(frutas)
+
+frutas.unshift("Melón")
+console.log(frutas)
+
+frutas.shift("Melón")
+console.log(frutas)
+
+var pos = frutas.indexOf("Plátano")
+console.log(pos)
