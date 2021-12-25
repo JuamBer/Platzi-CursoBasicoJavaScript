47aa36ba108679cea17b24f178d27bb90cdbe00c
Juan Sáez García <contacto@juamber.com>
2021-12-25 13:49:32 +0100

    aab_funciones

---
+++ aab_funciones/main.js
@@ -0,0 +1,15 @@
+//Declarativas
+
+function saludar(nombre){
+    console.log(`Hola ${nombre}`);
+}
+
+saludar("Ana")
+
+//Expresión u Anónima
+
+var miFuncionAnonima = function(a,b){
+    return a+b;
+}
+
+miFuncionAnonima(6,7)
