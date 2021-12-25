64ec5f2322b318f309395f1ecea8b38797b50f80
Juan Sáez García <contacto@juamber.com>
2021-12-25 14:15:20 +0100

    aaf_operadores

---
+++ aaf_operadores/main.js
@@ -0,0 +1,24 @@
+//Operadores Binarios
+3 + 2 // 5
+50 - 10 // 40
+10 * 20 // 200
+20 / 2 // 10
+
+"Diego " + "De Granda" // "Diego De Granda"
+
+//Operadores Unitarios
+!false //True
+
+var a = 1
+a++
+a+=2
+
+//Comparar
+3 == "3" // True
+3 === "3" // False
+
+5 < 3 // True
+5 <=5 // True
+
+true && true //True
+true || false //True
