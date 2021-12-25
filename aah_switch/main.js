9d89a84ae5868d762a4c2efb0577ed3b7b4f6003
Juan Sáez García <contacto@juamber.com>
2021-12-25 15:21:44 +0100

    aah_switch

---
+++ aah_switch/main.js
@@ -0,0 +1,13 @@
+
+var a = 21;
+switch(a){
+    case 1:
+        console.log("Soy Uno!")
+        break
+    case 2:
+        console.log("Soy Dos!")
+        break
+    default:
+        console.log("No Soy Ninguno")
+        break
+}
