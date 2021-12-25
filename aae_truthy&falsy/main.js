5b533dbebfde86b5d5ff6ba3e2d9e86cc245c246
Juan Sáez García <contacto@juamber.com>
2021-12-25 14:07:13 +0100

    aae_truthy&falsy

---
+++ aae_truthy&falsy/main.js
@@ -0,0 +1,16 @@
+Boolean(0) // False
+Boolean(1) // True
+
+Boolean(null) // False
+Boolean(NaN) // False
+Boolean(undefined) // False
+
+Boolean(true) // True
+Boolean(false) // False
+
+Boolean("") // False
+Boolean("a") // True
+
+Boolean([]) //True
+
+Boolean(function(){}) //True
