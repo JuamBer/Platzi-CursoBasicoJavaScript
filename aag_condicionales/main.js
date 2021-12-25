939bd5901e0486ebf42b24658b36c0ea0a0753cc
Juan Sáez García <contacto@juamber.com>
2021-12-25 14:23:28 +0100

    aag_condicionales

---
+++ aag_condicionales/main.js
@@ -0,0 +1,25 @@
+
+if(true){
+    console.log("Hola")
+}
+
+if (false) {
+    console.log("Adios")
+} else {
+    console.log("Soy Falso")
+}
+
+var edad = 18
+
+if (edad === 18) {
+    console.log("Puedes votar, será tu primera vez")
+} else if(edad > 18){
+    console.log("Puedes votar")
+} else {
+    console.log("No pedes votar")
+}
+
+//condition ? true : false
+var n1 = 1
+var res = n1 === 1 ? "Soy un uno" : "No soy un uno"
+res
